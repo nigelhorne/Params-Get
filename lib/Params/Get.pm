ca2270920ab6ce1e40f4c3abb45dcf7fd6a1fbcf
Nigel Horne <njh@bandsman.co.uk>
2025-05-22 16:52:05 -0400

    Release 0.06

--- lib/Params/Get.pm
+++ lib/Params/Get.pm
@@ -15,11 +15,11 @@ Params::Get - Get the parameters to a subroutine in any way you want
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
